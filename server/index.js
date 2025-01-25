6f82cfa9b256c5967b39b1d8130d963853fdd33e
utkarsh2023021 <168927514+utkarsh2023021@users.noreply.github.com>
2025-01-26 03:05:24 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -55,7 +55,7 @@ const upload = multer({ storage });
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:5000/auth/google/callback',
+  callbackURL: 'https://pdforganizer.onrender.com/auth/google/callback',
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     console.log("🔄 Google Profile Received:", profile.displayName);
